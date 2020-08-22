7ab948d8ee2c42aad316ced34d89263dd38179cf
jaimegsandoval <jaime.devs@gmail.com>
2020-08-22 00:15:12 -0700

    worked on selecting the correct data from the astronomy srtcle search;

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-// $.ajax({
-//   // url: "https://holidayapi.com/v1/holidays?pretty&key=9c40cd28-12d7-45c8-aecc-00c4d2a5e54c&country=" + countryCode + "&year=2019",
-//   // headers: {
-//   //   "x-rapidapi-host": "newscatcher.p.rapidapi.com",
-//   //   "x-rapidapi-key": "6bb1f7d518mshee6c717c3746b3ap119550jsned3e9335e862"
-//   // },
-//   headers: {
-//     "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-//     "x-rapidapi-key": "6bb1f7d518mshee6c717c3746b3ap119550jsned3e9335e862"
-//   },
-//   url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/NewsSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q=Astronomy&safeSearch=false",
-//   method: "GET",
-//   success: handleGetHolidaySuccess,
-//   error: handleGetHolidayError
-// })
+$.ajax({
+  // url: "https://holidayapi.com/v1/holidays?pretty&key=9c40cd28-12d7-45c8-aecc-00c4d2a5e54c&country=" + countryCode + "&year=2019",
+  // headers: {
+  //   "x-rapidapi-host": "newscatcher.p.rapidapi.com",
+  //   "x-rapidapi-key": "6bb1f7d518mshee6c717c3746b3ap119550jsned3e9335e862"
+  // },
+  headers: {
+    "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+    "x-rapidapi-key": "6bb1f7d518mshee6c717c3746b3ap119550jsned3e9335e862"
+  },
+  url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/NewsSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q=Astronomy&safeSearch=false",
+  method: "GET",
+  success: handleGetHolidaySuccess,
+  error: handleGetHolidayError
+})
 
 
 
-// function handleGetHolidaySuccess(response) {
-//   console.log(response);
-//   console.log(response.value[0].title);
-//   console.log(response.value[0].url)
-//   for (let i = 0; i < response.value.length; i++) {
-//     console.log(response.value[i].title)
-//   }
-// }
+function handleGetHolidaySuccess(response) {
+  console.log(response);
+  for (let i = 0; i < response.value.length; i++) {
+    console.log('title', response.value[i].title);
+    console.log('description', response.value[i].description);
+    console.log('provider', response.value[i].provider);
+    console.log('date published', response.value[i].datePublished);
+    console.log('url', response.value[i].url);
+  }
 
+}
 
-// function handleGetHolidayError(error) {
-//   console.log(error);
-// }
+function handleGetHolidayError(error) {
+  console.log(error);
+}
 
 
 
@@ -44,6 +46,7 @@ const heroSpaceImg = document.querySelector('.banner-img');
 function handleGetHubbleImgSuccess(response) {
   const randomSpaceImg = Math.round((Math.random() * 20)) + 1;
   for (let i = 0; response.hits.length; i++) {
+    console.log(response.hits[i]);
     if (!response) {
       heroSpaceImg.style.backgroundImage = 'url("images/default-hero-img.jpg")';
       return;
@@ -51,14 +54,8 @@ function handleGetHubbleImgSuccess(response) {
       heroSpaceImg.style.backgroundImage = "url(" + response.hits[randomSpaceImg].webformatURL + ")";
       return;
     }
-    // console.log(response.hits);
 
   }
-  // console.log(response.value[0].title);
-  // console.log(response.value[0].url)
-  // for (let i = 0; i < response.value.length; i++) {
-  //   console.log(response.value[i].title)
-  // }
 }
 
 
