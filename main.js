47c37dc41b2cd9de096adbc9289488c6f7acf755
jaimegsandoval <jaime.devs@gmail.com>
2020-08-30 12:11:26 -0700

    Changed event listenrs from touchstart to click

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ function renderNewPage(e) {
     return;
   }
 
-  footerContainer.removeEventListener('touchstart', renderNewPage, false);
+  footerContainer.removeEventListener('click', renderNewPage, false);
   sideNavMobile.removeEventListener('click', renderNewPage, false);
 
   sideModalOverlay.classList.remove('sideNav');
@@ -140,7 +140,7 @@ function handleGetDataSuccess(response) {
 
 function handleGetDataError(error) {
   sideNavMobile.addEventListener('click', renderNewPage);
-  footerContainer.addEventListener('touchstart', renderNewPage);
+  footerContainer.addEventListener('click', renderNewPage);
   const errorText = document.createElement('h2');
   errorText.textContent = "An Error has Occurred. Please try again.";
   errorText.classList.add('error-text');
@@ -222,7 +222,7 @@ function renderArticle(articleData) {
   }
 
   loader.classList.add('hidden');
-  footerContainer.addEventListener('touchstart', renderNewPage);
+  footerContainer.addEventListener('click', renderNewPage);
   sideNavMobile.addEventListener('click', renderNewPage);
   greetModal();
 }
@@ -251,7 +251,7 @@ function addCurrentPageClass(e) {
 }
 
 setTimeout(function () {
-  return footerContainer.addEventListener('touchstart', renderNewPage);
+  return footerContainer.addEventListener('click', renderNewPage);
 }, 4000);
 
 
