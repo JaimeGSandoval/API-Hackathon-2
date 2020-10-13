db2745a8846c8cb295b20492ec07d70f521a7972
jaimegsandoval <jaime.devs@gmail.com>
2020-10-13 16:35:59 -0700

    Fixed landscape scroll for side nav

--- main.js
+++ main.js
@@ -51,20 +51,7 @@ const headerTitles = {
   "d-wave": "Quantum Computing"
 }
 
-var month = [];
-month[0] = "Jan";
-month[1] = "Feb";
-month[2] = "Mar";
-month[3] = "Apr";
-month[4] = "May";
-month[5] = "Jun";
-month[6] = "Jul";
-month[7] = "Aug";
-month[8] = "Sept";
-month[9] = "Oct";
-month[10] = "Nov";
-month[11] = "Dec";
-
+var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 function start() {
   renderImage();
